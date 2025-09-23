2d7c91749348ae3488d42d2311cf255abf27ceae
Jefferson Correa <103817130+Jxfferson@users.noreply.github.com>
2025-09-23 14:45:28 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pathlib import Path
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles  # <-- Importa StaticFiles
 from backend.db.database import Base, engine
@@ -18,19 +19,21 @@ from backend.controllers.expediente_controller import router as expediente_route
 from backend.controllers.moderador_controller import router as moderador_router
 from backend.controllers.intercambio_controller import router as intercambio_router
 from backend.controllers import intercambio_habilidad_controller
-from backend.controllers import notificacion_controller
 from backend.controllers.chat_controller import router as chat_router 
+from backend.controllers.call_controller import router as call_router
 from backend.controllers.user_admin_controller import router as userA_router
 from backend.controllers import intercambio_admin_controller
 from backend.controllers import perfil_admin_controller
 from backend.controllers import publicaciones_admin_controller
-from backend.controllers.inscripcion_curso_controller import router as inscripcion_curso_router
+from backend.controllers import notificacion_controller
+
 
 app = FastAPI()
 
 # Configuración CORS
 origins = [
-    "http://localhost:3000",  # tu frontend
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",# tu frontend
 ]
 
 app.add_middleware(
@@ -41,11 +44,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Ruta absoluta a la carpeta 'uploads' dentro de backend
+BASE_DIR = Path(__file__).resolve().parent  # backend/
+UPLOADS_DIR = BASE_DIR / "uploads"          # backend/uploads/
+
 # Base de datos
 Base.metadata.create_all(bind=engine)
-
-# Montar carpeta uploads como estática para servir archivos
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 # Rutas
 app.include_router(auth_router, prefix="/auth")
@@ -63,13 +67,15 @@ app.include_router(expediente_router)
 app.include_router(moderador_router)
 app.include_router(intercambio_router)
 app.include_router(intercambio_habilidad_controller.router)
-app.include_router(notificacion_controller.router)
 app.include_router(chat_router)
+app.include_router(call_router)
+app.include_router(notificacion_controller.router)
 app.include_router(userA_router)
 app.include_router(curso_controller.router, prefix="/admin")
 app.include_router(intercambio_admin_controller.router)
 app.include_router(perfil_admin_controller.router)
 app.include_router(publicaciones_admin_controller.router)
-app.include_router(inscripcion_curso_router,  tags=["inscripciones_cursos"])
-print("✅ Router publicaciones_admin_controller registrado")
 
+
+# Servir archivos estáticos
+app.mount("/uploads", StaticFiles(directory=str(UPLOADS_DIR)), name="uploads")
