51df8f09f2027f8ae045635693b9af5f51093429
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 13:37:30 -0500

    Delete frontend/src/pages/main.tsx

--- frontend/src/pages/main.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './auth/login';
-import "@/pages/index.css"
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(<App />);
