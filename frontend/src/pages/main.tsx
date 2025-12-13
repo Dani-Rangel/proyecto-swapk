c55a0d9bcfef069a6a93876153146edd107a918f
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 13:33:45 -0500

    Update main.tsx

--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './auth/login';
-import styles from "./index.css"
+import "@/pages/index.css"
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
