aa8cfe40a6605877e18f893d96e9b52b5ba05033
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 08:04:46 -0500

    Update database connection string to use env variables
    Refactor connection string to use environment variables for configuration.

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,16 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
+# Obtener las variables de entorno
+host = os.getenv("MYSQLHOST", "localhost")
+user = os.getenv("MYSQLUSER", "root")
+password = os.getenv("MYSQLPASSWORD", "")
+database = os.getenv("MYSQLDATABASE", "swapk")
+port = os.getenv("MYSQLPORT", "3306")
 
-#Cadena de conexion
-#MARIADB_URL = 'mysql+pymysql://root:admin@localhost:3315/swapk'
-MARIADB_URL = 'mysql+pymysql://root:@localhost:3306/swapk'
-#Crear el objeto de conexion
+# Construir la URL de conexión
+MARIADB_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+
+# Crear el objeto de conexión
 engine = create_engine(MARIADB_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
