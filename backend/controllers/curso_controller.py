4160341c64b3876ea9c66317a75c23666abcfa69
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 22:58:19 -0500

    Update curso_controller.py

--- backend/controllers/curso_controller.py
+++ backend/controllers/curso_controller.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
-from backend.models.Inscripciones_Cursos import InscripcionCurso as InscripcionCursoModel
+from models.Inscripciones_Cursos import InscripcionCurso as InscripcionCursoModel
 
 from db.database import get_db
 from schemas.curso_schema import CursoCreate, CursoUpdate, CursoResponse
