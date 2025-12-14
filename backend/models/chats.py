08ee2a86b47cc10d8f5254d86ceaec1605a86088
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 22:41:11 -0500

    Update chats.py

--- backend/models/chats.py
+++ backend/models/chats.py
@@ -1,4 +1,4 @@
-from backend.db import Base
+from db import Base
 from sqlalchemy import Column, Integer, String, DateTime, Enum, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
