997f6279f972ed997813d4595d57b41692ba36ca
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 22:37:41 -0500

    Update sessions.py

--- backend/db/sessions.py
+++ backend/db/sessions.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import sessionmaker
-from backend.db.database import engine
+from db.database import engine
 
 SessionLocal = sessionmaker(autocommit= False,
                             autoflush= False,
