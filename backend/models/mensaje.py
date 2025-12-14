011e0cf6d199f4caa2be47614918a3ce64573bbc
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 22:43:21 -0500

    Update mensaje.py

--- backend/models/mensaje.py
+++ backend/models/mensaje.py
@@ -1,4 +1,4 @@
-from backend.db import Base
+from db import Base
 from sqlalchemy import Column, Integer, Text, DateTime, Boolean, String, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
