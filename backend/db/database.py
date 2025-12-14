5a9992c31355f8909b7abeb45d4f1069f76e8642
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-14 10:29:39 -0500

    Update database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,17 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Obtener las variables de entorno
+# ✅ OBTENER VARIABLES DE ENTORNO (¡NUNCA USAR "localhost"!)
 host = os.getenv("MYSQLHOST", "mysql.railway.internal")  # ✅ Por defecto, el correcto
 user = os.getenv("MYSQLUSER", "root")
 password = os.getenv("MYSQLPASSWORD", "")
 database = os.getenv("MYSQLDATABASE", "railway")
 port = os.getenv("MYSQLPORT", "3306")
 
-# Construir la URL de conexión
+# ✅ CONSTRUIR LA URL DE CONEXIÓN
 MARIADB_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
-# Crear el objeto de conexión
+# ✅ CREAR CONEXIÓN
 engine = create_engine(MARIADB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
