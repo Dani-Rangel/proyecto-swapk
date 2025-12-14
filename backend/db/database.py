9dbce83f3f3f5482c7ba933b29acda9ed11f3349
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 20:56:28 -0500

    Update database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,10 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_SWAPK_URL = os.getenv("DB_SWAPK_URL")
 
 engine = create_engine(
-    DATABASE_URL,
+    DB_SWAPK_URL,
     pool_pre_ping=True
 )
 
