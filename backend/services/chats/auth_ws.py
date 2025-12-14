66554f11443da0eb067efa85e3c798984e1f4db5
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 22:46:31 -0500

    Update auth_ws.py

--- backend/services/chats/auth_ws.py
+++ backend/services/chats/auth_ws.py
@@ -1,8 +1,8 @@
 # backend/services/auth_ws.py
 from jose import jwt, JWTError
-from backend.config import SECRET_KEY, ALGORITHM
-from backend.db.database import get_db
-from backend.models.usuarios import Usuario
+from config import SECRET_KEY, ALGORITHM
+from db.database import get_db
+from models.usuarios import Usuario
 
 def get_user_from_token_sync(token: str, db):
     try:
