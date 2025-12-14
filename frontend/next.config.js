e4f49ef4394ef79bdcb1b044ba3ebd7850d0a824
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-14 15:48:47 -0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dribbble.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
-
