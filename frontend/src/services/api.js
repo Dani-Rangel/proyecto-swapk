3bc22daf731aabfd34d37f9a097e64c26566c08d
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-14 15:56:28 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL = "http://localhost:3000";
 
 export async function registerUser(data) {
   const res = await fetch(`${API_URL}/register`, {
