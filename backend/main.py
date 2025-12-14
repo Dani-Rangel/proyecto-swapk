098e0d7fe76be991eb468f8a890de27d6614c7be
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 21:25:34 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,9 @@ from pathlib import Path
 import sys
 import os
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, os.pardir)) 
-sys.path.append(project_root)
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# project_root = os.path.abspath(os.path.join(current_dir, os.pardir)) 
+# sys.path.append(project_root)
 
 from backend.db.database import Base, engine, get_db
 
