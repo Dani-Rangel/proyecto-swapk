e0070ba73707e55ec67b814aa527eaa81acb081a
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-13 21:18:30 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,6 @@ import sys
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-# Si main.py está en swapk_proyecto/backend/, subimos un nivel para llegar a swapk_proyecto/
 project_root = os.path.abspath(os.path.join(current_dir, os.pardir)) 
 sys.path.append(project_root)
 
