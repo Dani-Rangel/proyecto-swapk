103a7c20f7544f7e4b20d3b6f233b5d1e8998478
Dani-Rangel <alejorangel2008@gmail.com>
2025-12-15 15:43:37 -0500

    Update api_cursoHabilidad.ts

--- frontend/src/services/api_cursoHabilidad.ts
+++ frontend/src/services/api_cursoHabilidad.ts
@@ -1,7 +1,7 @@
 // services/api_CursoHabilidad.ts
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://backend-production-fc5e.up.railway.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
